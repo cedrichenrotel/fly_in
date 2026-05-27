3da288c2de1fe989016d8776310f2b7ff84120ed
cedrichenrotel <cedric.henrotel@gmail.com>
2026-05-27 11:45:40 +0200

    correction du main

--- src/main.py
+++ src/main.py
@@ -13,12 +13,10 @@ def main() -> None:
     window = Window()
     arcade.run()
 
-    turns = 0
     if window.simulator:
         for turn in window.simulator.stock_turns:
-            print(f"turn {turns}: {', '.join(turn)}")
+            print(' '.join(turn))
             print()
-            turns += 1
 
 
 if __name__ == "__main__":
